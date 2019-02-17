7aec97857e720db2c9076f783dfb2bd4e0513961
mischoko <michal.zak.misch@gmail.com>
2019-02-17 15:18:42 +0100

    fixed js

--- js/main.js
+++ js/main.js
@@ -33,10 +33,24 @@ window.onscroll = function() {
  document.querySelector('.menuContBtn').addEventListener("click", function(event) {
     event.preventDefault();
     var offset = 0, y = 0, dy, call = setInterval(function(){
-    	if( Math.abs(dy=offset-y)>1 ) y += dy/8;
+    	if( Math.abs(dy=offset-y)>1 ) y += dy;
       else { clearInterval(call); y = offset; }
       document.documentElement.scrollTop = y;
     },10);
     offset = document.getElementById(event.srcElement.dataset.scroll).offsetTop;
 		y = document.documentElement.scrollTop;
 });
+
+
+function fun() {
+  var checkbox = document.getElementById('navClick');
+  var bug = document.getElementById('mobileMenu');
+  var contact = document.getElementsByClassName('menuItem')[4];
+  if (checkbox.checked == true){
+    bug.classList.remove('hidden');
+    contact.addEventListener("click", function(){
+        checkbox.checked = false;
+    })
+  } else {
+  }
+}
